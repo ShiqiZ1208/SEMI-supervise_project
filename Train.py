592391bc95c74340d6cc2089fb2ca9766d47dc43
ShiqiZ1208 <ZHANG3T3@Uwindsor.ca>
2025-12-01 11:40:06 -0500

    Update Train.py

--- Train.py
+++ Train.py
@@ -14,6 +14,7 @@ from ganmodel import BART_base_model, Roberta_discriminator, classification_loss
 import numpy as np
 import torch.nn.functional as F
 import os
+import random
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
@@ -21,7 +22,7 @@ def train_model(n_epochs, minibatch_sizes, is_save, is_load, load_pathG, load_pa
 
 ########################################## load the tokenizer and model ##################################################
     # load model ckpt from huggingface and use it to tokenizer
-    threshold = 0.7
+    threshold = 0.75
     fine_tuning = True
     BART_model_ckpt = 'facebook/bart-base'
     RoBERTa_model_ckpt = "roberta-base"
@@ -58,27 +59,27 @@ def train_model(n_epochs, minibatch_sizes, is_save, is_load, load_pathG, load_pa
     # set up learning schedualer for both discriminator and generator
     num_training_steps = num_epochs * len(train_dataloader)
     num_warm_up = 100
-    lr_schedulerG = get_scheduler(
-        name="linear", optimizer=optimizerG, num_warmup_steps=num_warm_up, num_training_steps=num_training_steps
-    )
-    lr_schedulerD = get_scheduler(
-        name="linear", optimizer=optimizerD, num_warmup_steps=num_warm_up, num_training_steps=num_training_steps
-    )
+    #lr_schedulerG = get_scheduler(
+        #name="linear", optimizer=optimizerG, num_warmup_steps=num_warm_up, num_training_steps=num_training_steps
+    #)
+    #lr_schedulerD = get_scheduler(
+        #name="linear", optimizer=optimizerD, num_warmup_steps=num_warm_up, num_training_steps=num_training_steps
+    #)
     if is_load:
       optimizerD.load_state_dict(ckptD['optimizer_state'])
       optimizerG.load_state_dict(ckptG['optimizer_state'])
-      lr_schedulerD.load_state_dict(ckptD['lr_scheduler'])
-      lr_schedulerG.load_state_dict(ckptG['lr_scheduler'])
+      #lr_schedulerD.load_state_dict(ckptD['lr_scheduler'])
+      #lr_schedulerG.load_state_dict(ckptG['lr_scheduler'])
     if fine_tuning:
-      fine_tune_model(NetG, NetD, device, 2, minibatch_sizes, train_dataloader, eval_dataloader, optimizerG, optimizerD, lr_schedulerG, lr_schedulerD, RBE_tokenizer, BA_tokenizer, is_save, rouge)
+      fine_tune_model(NetG, NetD, device, 2, minibatch_sizes, train_dataloader, eval_dataloader, optimizerG, optimizerD, RBE_tokenizer, BA_tokenizer, is_save, rouge)
     small_train_dataloader = DataLoader(t_dataset, shuffle=False, batch_size=int(minibatch_sizes/2), worker_init_fn=lambda worker_id: np.random.seed(seed))
     for epoch in range(num_epochs):
       pseudo_dataloader = generate_pseudo_label(NetD, NetG, RBE_tokenizer, u_t_dataset, minibatch_sizes, threshold, seed)
       combine_dataset_training(epoch, small_train_dataloader, pseudo_dataloader, eval_dataloader, minibatch_sizes, NetG, NetD, optimizerD, optimizerG, RBE_tokenizer, BA_tokenizer, is_save, rouge)
-      threshold = threshold + 0.05
 
 
-def fine_tune_model(NetG, NetD, device, num_epochs, minibatch_sizes, train_dataloader, eval_dataloader, optimizerG, optimizerD, lr_schedulerG, lr_schedulerD, RBE_tokenizer, BA_tokenizer, is_save, rouge):
+
+def fine_tune_model(NetG, NetD, device, num_epochs, minibatch_sizes, train_dataloader, eval_dataloader, optimizerG, optimizerD, RBE_tokenizer, BA_tokenizer, is_save, rouge):
     print("\n=============================================fine tuning==================================")
     num_training_steps = num_epochs * len(train_dataloader)
     print(f"\nNum_Epochs:{num_epochs}, Batch_size:{minibatch_sizes}")
@@ -153,12 +154,12 @@ def fine_tune_model(NetG, NetD, device, num_epochs, minibatch_sizes, train_datal
             preds = (torch.sigmoid(flogits.logits.detach()) <= 0.5).long()
             accuracyF = (preds == labels).float().mean()
             A_f.append(accuracyF)
-            alpha = 0.6
+            alpha = 0.5
             Dloss = alpha*loss1 + (1-alpha)*loss2
             Dloss.backward()
-            clip_grad_norm_(NetD.parameters(), max_norm=1.0)
+            #clip_grad_norm_(NetD.parameters(), max_norm=1.0)
             optimizerD.step()
-            lr_schedulerD.step()
+            #lr_schedulerD.step()
 
             #for p in NetG.parameters():
                 #p.requires_grad = True
@@ -173,9 +174,9 @@ def fine_tune_model(NetG, NetD, device, num_epochs, minibatch_sizes, train_datal
 
                 Gloss = bart_loss
                 Gloss.backward()
-                clip_grad_norm_(NetG.parameters(), max_norm=1.0)
+                #clip_grad_norm_(NetG.parameters(), max_norm=1.0)
                 optimizerG.step()
-                lr_schedulerG.step()
+                #lr_schedulerG.step()
                 
                 for p in NetD.parameters():
                     p.requires_grad = True
@@ -229,7 +230,7 @@ def fine_tune_model(NetG, NetD, device, num_epochs, minibatch_sizes, train_datal
 
         print("\nEpoch:{: <5}| validation_loss:{: <5.4f}".format(epochs, average_loss))
         print("\nrouge1:{: <5.4f}| rouge2:{: <5.4f}| rougeL:{: <5.4f}| rougeLsum:{: <5.4f}".format(r_score['rouge1'], r_score['rouge2'], r_score['rougeL'], r_score['rougeLsum']))
-        rouge_list = [r_score['rouge1'], r_score['rouge2'], r_score['rougeL'], r_score['rougeLsum']]
+        #rouge_list = [r_score['rouge1'], r_score['rouge2'], r_score['rougeL'], r_score['rougeLsum']]
         #Rouge_record.append(rouge_list)
         print(f"\n=============================End Validation for fine tune Epoch: {epochs}==================================")
 
@@ -241,14 +242,14 @@ def fine_tune_model(NetG, NetD, device, num_epochs, minibatch_sizes, train_datal
           torch.save({
               "model_state": NetG.state_dict(),
               "optimizer_state": optimizerG.state_dict(),
-              "lr_scheduler": lr_schedulerG.state_dict(),
+              #"lr_scheduler": lr_schedulerG.state_dict(),
               "epoch": epoch
           }, G_path)
 
           torch.save({
               "model_state": NetD.state_dict(),
               "optimizer_state": optimizerD.state_dict(),
-              "lr_scheduler": lr_schedulerD.state_dict(),
+              #"lr_scheduler": lr_schedulerD.state_dict(),
               "epoch": epoch
           }, D_path)
     print("\n=============================================end fine tuning==================================")
@@ -267,6 +268,7 @@ def generate_pseudo_label(NetD, NetG, RBE_tokenizer, u_t_dataset, minibatch_size
     psudo_ids = []
     psudo_label = []
     psudo_mask = []
+    summary_score = []
     breaks = 0
     num_pass = 0
     test_flag = 0
@@ -298,7 +300,7 @@ def generate_pseudo_label(NetD, NetG, RBE_tokenizer, u_t_dataset, minibatch_size
         flogits = NetD(roberta_finput_ids, attention_mask=roberta_fattention_masks)
         pred = torch.sigmoid(flogits.logits.detach())
         #print(pred.shape)
-        threshold = 0.7
+        #threshold = 0.7
         mask = pred > threshold
         passed = mask.sum().item()
         mask = mask.squeeze(-1)
@@ -307,18 +309,30 @@ def generate_pseudo_label(NetD, NetG, RBE_tokenizer, u_t_dataset, minibatch_size
         padded_output_ids = F.pad(genrated, (0, 512 - genrated.shape[1]), value=1).detach()
         #print(padded_output_ids.shape)
         #label_list = padded_output_ids[mask].tolist()
+        score_list = [seq for seq in pred[mask]]
         label_list = [seq for seq in padded_output_ids[mask]]
         mask_list = [seq for seq in attention_mask[mask]]
         ids_list = [seq for seq in input_ids[mask]]
         psudo_label.extend(label_list)
         psudo_mask.extend(mask_list)
         psudo_ids.extend(ids_list)
+        summary_score.extend(score_list)
         progress_bar.update(1)
         #test_flag += 1
         #if test_flag == 80:
           #break
     print("\n========================================end generate pseudo label==================================")
     print("number of labels generated: ", len(psudo_ids))
+    combined = list(zip(summary_score, psudo_label, psudo_ids, psudo_mask))
+    combined_sorted = sorted(combined, key=lambda x: x[0], reverse=True)
+    if len(combined_sorted) > 800:
+        combined_sorted = combined_sorted[:800]
+    random.shuffle(combined_sorted)
+    summary_score, psudo_label, psudo_ids, psudo_mask = zip(*combined_sorted)
+    summary_score = list(summary_score)
+    psudo_label = list(psudo_label)
+    psudo_ids = list(psudo_ids)
+    psudo_mask = list(psudo_mask)
     pseudo_data = pseudoDataset(psudo_label,psudo_mask,psudo_ids)
     p_dataset = DataLoader(pseudo_data, shuffle=False, batch_size=int(minibatch_sizes/2), worker_init_fn=lambda worker_id: np.random.seed(seed))
     for param in NetG.parameters():
@@ -372,7 +386,7 @@ def combine_dataset_training(epochs, train_dataloader, pseudo_dataloader, eval_d
             num_beams=4,
             early_stopping=True
             )   
-        s_token_id = torch.tensor(0, device=device)
+        #s_token_id = torch.tensor(0, device=device)
         genrated = genrated[:, 1:]
 
         genrated = genrated.detach()
@@ -384,7 +398,7 @@ def combine_dataset_training(epochs, train_dataloader, pseudo_dataloader, eval_d
         roberta_tinput_ids, roberta_tattention_masks = concatenate_summary_keyword(discrimnator_input_id, discrimnator_mask,batch_knoun,batch_knoun_mask,device,RBE_tokenizer)
 
 
-        attention_mask_hidden = torch.ones(int(minibatch_sizes/2), 256).long()
+        #attention_mask_hidden = torch.ones(int(minibatch_sizes/2), 256).long()
         flogits = NetD(roberta_finput_ids, attention_mask=roberta_fattention_masks)
         tlogits = NetD(roberta_tinput_ids, attention_mask=roberta_tattention_masks)
 
@@ -397,10 +411,10 @@ def combine_dataset_training(epochs, train_dataloader, pseudo_dataloader, eval_d
         preds = (torch.sigmoid(flogits.logits.detach()) <= 0.5).long()
         accuracyF = (preds == labels).float().mean()
         A_f.append(accuracyF)
-        alpha = 0.6
+        alpha = 0.5
         Dloss = alpha*loss1 + (1-alpha)*loss2
         Dloss.backward()
-        clip_grad_norm_(NetD.parameters(), max_norm=1.0)
+        #clip_grad_norm_(NetD.parameters(), max_norm=1.0)
         optimizerD.step()
         #lr_schedulerD.step()
 
@@ -415,9 +429,9 @@ def combine_dataset_training(epochs, train_dataloader, pseudo_dataloader, eval_d
         supervised_loss = output1_g.loss
         output2_g = NetG(input_ids=pseudo_ids, attention_mask=pseudo_attention_mask, labels = pseudo_labels)
         unsupervised_loss = output2_g.loss
-        Gloss = supervised_loss + 0.7 * unsupervised_loss
+        Gloss = supervised_loss + 0.2 * unsupervised_loss
         Gloss.backward()
-        clip_grad_norm_(NetG.parameters(), max_norm=1.0)
+        #clip_grad_norm_(NetG.parameters(), max_norm=1.0)
         optimizerG.step()
         #lr_schedulerG.step()
         for p in NetD.parameters():
